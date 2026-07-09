2ac40870db2a0bb568507840522654164044020d
Jgon7 <jessica.gonzalez0710@gmail.com>
2026-07-09 01:21:49 -0300

    se agrega el modulo de curso

--- obligatorioP3/Curso.h
+++ obligatorioP3/Curso.h
@@ -1,6 +1,18 @@
 #ifndef CURSO_H_INCLUDED
 #define CURSO_H_INCLUDED
+#include "Fecha.h"
+#include "String.h"
+#include "Asignatura.h"
 
+typedef struct {
+    int numeroAsignatura;
+    String nombreAsignatura;
+    Fecha fecha;
+    int Calificacion;
+} Curso;
+
+void crearCurso(Curso &C, int numeroA, String nombreA, Fecha fecha, int calificacion);
+String obtenerNombre(Curso curso);
 
 
 #endif // CURSO_H_INCLUDED
