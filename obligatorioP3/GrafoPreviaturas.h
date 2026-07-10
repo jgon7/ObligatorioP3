436a830ee1d79fdfde397ef26cd608ffa081a51c
Facundo Sander <62453096+facundosander@users.noreply.github.com>
2026-07-10 19:25:06 -0300

    Change ExistePreviatura return type to bool

--- obligatorioP3/GrafoPreviaturas.h
+++ obligatorioP3/GrafoPreviaturas.h
@@ -6,7 +6,7 @@ typedef int Grafo[cantAsignaturas][cantAsignaturas];
 
 void CrearGrafo (Grafo &G);
 void AgregarPreviatura(Grafo &G, int asig1, int asig2);
-void ExistePreviatura(Grafo &G, int asig1, int asig2);
+bool ExistePreviatura(Grafo &G, int asig1, int asig2);
 void listarPreviaturas();
 
 #endif // GRAFOPREVIATURAS_H_INCLUDED
