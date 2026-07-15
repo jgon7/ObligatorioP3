693e926ea5663474ebd842b76fe6a53affc258a9
Jessica Gonzalez <84815093+jgon7@users.noreply.github.com>
2026-07-15 19:39:49 -0300

    Add files via upload

---
+++ obligatorioP3/Menu.h
@@ -0,0 +1,8 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+#include <iostream>
+using namespace std;
+
+void ejecutarMenu(Grafo &G, Asignaturas &AS, HashAlumnos &AL);
+
+#endif // MENU_H_INCLUDED
